eb659f48f9f233553176dd3f55145d3dc872b5f5
SELISE\adib.mehedi <adibmehedi@gmail.com>
2017-11-07 17:48:38 +0600

    Demo added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
 (function(){
     "use strict";
+    angular
+        .module('WeatherApp',[])
+        .controller('WeatherController',WeatherController);
 
+    WeatherController.$inject=['$scope','$http'];
 
+    function WeatherController($scope,$http){
+        var vm=this;
+        $scope.showResult=false;
+        $scope.locationInput;
+
+        $scope.getWeatherInformation=function(){
+            $scope.showResult=true;
+            $APIkey='';
+            $baseUrl='http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1'
+
+            $http({
+                method: 'GET',
+                url: 'https://raw.githubusercontent.com/typicode/json-server/master/package-lock.json'
+              }).then(function successCallback(response) {
+                  console.log(response.data);
+                }, function errorCallback(response) {
+                  console.log("Ajax req error");
+                });
+             
+                //console.log("User Input: ",$scope.locationInput);
+        }
+
+        //console.log("App Context: ",$scope);
+    }
 })();
 
 
