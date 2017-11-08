acdb57ac43079a14f5047e8b5b526f6d926eb036
SELISE\adib.mehedi <adibmehedi@gmail.com>
2017-11-08 20:21:11 +0600

    Event handling through rootScope

--- js/main.js
+++ js/main.js
@@ -2,40 +2,103 @@
     "use strict";
     angular
         .module('WeatherApp', [])
-        .controller('WeatherController', WeatherController);
+        .controller('WeatherDisplayController', WeatherDisplayController)
+        .controller('WeatherInputController', WeatherInputController)
+        .service('WeatherInfoService', WeatherInfoService);
 
-    WeatherController.$inject = ['$scope', '$http'];
+    /*
+    * Weather Input controller
+    */
+    WeatherInputController.$inject = ['$scope','WeatherInfoService', '$rootScope'];
+    function WeatherInputController($scope, WeatherInfoService, $rootScope) {
+        var inputController = this;
 
-    function WeatherController($scope, $http) {
-        var vm = this;
-        $scope.showResult = false;
-        $scope.locationInput;
-        $scope.Weatherdata;
+        inputController.setLocation = function () {
+            WeatherInfoService.setLocation(inputController.locationInput);
+            //debugger;
+            $rootScope.$broadcast('location-set');
+        }
+    }
 
-        $scope.getWeatherInformation = function () {
-            $scope.showResult = true;
-            var APIkey = '11306b21803ed2e3a3006b36c271f5ec';
-            var baseUrl = "http://api.openweathermap.org/data/2.5/weather"
+    /*
+    * Weather Display controller
+    */
+    WeatherDisplayController.$inject = ['$scope','WeatherInfoService', '$rootScope'];
+    function WeatherDisplayController($scope, WeatherInfoService, $rootScope) {
 
-            $http({
-                method: 'GET',
-                url: (baseUrl),
-                params: {
-                    "q": $scope.locationInput,
-                    "appid": APIkey
-                }
-            },
-            ).then(function successCallback(response) {
-                console.log("Response:",response);
-                $scope.Weatherdata=response.data.main;
-                console.log($scope.Weatherdata);
+        $rootScope.$on('location-set', function () {
+            //debugger;
+            displayController.locationName=WeatherInfoService.getLocation();
+            displayController.showWeatherInformation();
+        })
 
+        var displayController = this;
+        displayController.showResult = false;
+        
+        displayController.locationName=WeatherInfoService.getLocation();
+       
+        displayController.showWeatherInformation = function () {
+            displayController.showResult = true;
+            
+            WeatherInfoService.fetchInfo().then(function successCallback(response) {
+                console.log("Response From Service:", response);
+                displayController.Weatherdata = response.data.main;
             }, function errorCallback(response) {
                 console.log("Ajax req error");
+                displayController.Weatherdata = {}
             });
+
         }
 
     }
+
+    /*
+    * Weather info service
+    */
+    WeatherInfoService.$inject = ['$http'];
+    function WeatherInfoService($http) {
+        var service = this;
+        var locationName;
+        var callBacks= {};
+
+        // service.setCallBack(callbackName, callbackFunction) {
+        //     if (!callBacks[callbackName]) {
+        //         callBacks[callbackName] = callbackFunction;
+        //     }
+        // }
+
+        // service.invokeCallBack(callbackName) {
+        //     callBacks[callBacks]();
+        // }
+
+        service.setLocation = function (name) {
+            //debugger;
+            locationName = name;
+            console.log("Location Set: ", locationName);
+        }
+
+        service.getLocation=function(){
+            //debugger;
+            console.log("Get locatio called:", locationName);
+            return locationName;
+        }
+
+        service.fetchInfo = function () {
+            var APIkey = '11306b21803ed2e3a3006b36c271f5ec';
+            var baseUrl = "http://api.openweathermap.org/data/2.5/weather"
+
+            return $http({
+                method: 'GET',
+                url: (baseUrl),
+                params: {
+                    "q": locationName,
+                    "appid": APIkey
+                }
+            },
+            );
+        }
+    }
+
 })();
 
 
