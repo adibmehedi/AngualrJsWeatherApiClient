3e3cf6c6c705d3f9a300250ee82718d5ac9ded6c
SELISE\adib.mehedi <adibmehedi@gmail.com>
2017-11-08 16:01:17 +0600

    http req updated

--- js/main.js
+++ js/main.js
@@ -1,34 +1,40 @@
-(function(){
+(function () {
     "use strict";
     angular
-        .module('WeatherApp',[])
-        .controller('WeatherController',WeatherController);
+        .module('WeatherApp', [])
+        .controller('WeatherController', WeatherController);
 
-    WeatherController.$inject=['$scope','$http'];
+    WeatherController.$inject = ['$scope', '$http'];
 
-    function WeatherController($scope,$http){
-        var vm=this;
-        $scope.showResult=false;
+    function WeatherController($scope, $http) {
+        var vm = this;
+        $scope.showResult = false;
         $scope.locationInput;
+        $scope.Weatherdata;
 
-        $scope.getWeatherInformation=function(){
-            $scope.showResult=true;
-            $APIkey='';
-            $baseUrl='http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1'
+        $scope.getWeatherInformation = function () {
+            $scope.showResult = true;
+            var APIkey = '11306b21803ed2e3a3006b36c271f5ec';
+            var baseUrl = "http://api.openweathermap.org/data/2.5/weather"
 
             $http({
                 method: 'GET',
-                url: 'https://raw.githubusercontent.com/typicode/json-server/master/package-lock.json'
-              }).then(function successCallback(response) {
-                  console.log(response.data);
-                }, function errorCallback(response) {
-                  console.log("Ajax req error");
-                });
-             
-                //console.log("User Input: ",$scope.locationInput);
+                url: (baseUrl),
+                params: {
+                    "q": $scope.locationInput,
+                    "appid": APIkey
+                }
+            },
+            ).then(function successCallback(response) {
+                console.log("Response:",response);
+                $scope.Weatherdata=response.data.main;
+                console.log($scope.Weatherdata);
+
+            }, function errorCallback(response) {
+                console.log("Ajax req error");
+            });
         }
 
-        //console.log("App Context: ",$scope);
     }
 })();
 
